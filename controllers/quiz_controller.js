70c0767279bf122610ac6a63091cdf1de260ea5c
Carlos Solsona <carlos.solsona@gmail.com>
2015-08-18 18:04:32 +0200

    Add comments

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,9 @@ var models = require('../models/models.js');
 var categorias = ["Otro", "Humanidades", "Ocio", "Ciencia", "Tecnología" ]; 
 
 exports.load = function(req, res, next, quizId){
-    models.Quiz.find(quizId).then(
+    models.Quiz.find({where: {id: Number(quizId)},
+                     include: [{model: models.Comment}]}
+        ).then(
         function(quiz){
             if(quiz){
                 req.quiz = quiz;
